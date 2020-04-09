b5444ba1e39063f86630e92010eef0f89885e1d5
Mateo Marulanda <marmacesal@gmail.com>
2020-04-09 13:32:18 -0500

    movement almost ready, we need to fix terrain limits and set a gravity

---
+++ Coffit/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public float horizontalMove;
+    public float verticalMove;
+    public CharacterController player;
+    public float playerSpeed;
+    void Start()
+    {
+        player = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalMove = Input.GetAxis("Horizontal");
+        verticalMove = Input.GetAxis("Vertical");
+
+    }
+
+    private void FixedUpdate()
+    {
+        player.Move(new Vector3(horizontalMove, 0, verticalMove) * playerSpeed * Time.deltaTime);
+    }
+}
