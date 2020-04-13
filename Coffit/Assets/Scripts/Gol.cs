dd5b56b280849ca4ef12f1c070c27b7927f54d85
yeshua19pro <yeshuaangelito@gmail.com>
2020-04-12 21:55:14 -0500

    agregando contador

--- Coffit/Assets/Scripts/Gol.cs
+++ Coffit/Assets/Scripts/Gol.cs
@@ -1,23 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Gol : MonoBehaviour {
     public Vector3 startPosition;
     public int cont = 0;
+    public Text puntuacion;
 
     void OnTriggerEnter (Collider other) {
-        if (cont == 0) {
+
             print ("Reset");
             startPosition = new Vector3 (0f, 2f, 5f);
             transform.position = startPosition;
             cont++;
-        }
+            puntuacion.text = "Puntuacion: "+ cont;
 
     }
     void OnTriggerExit (Collider other) {
-      
-        cont = 0;
+
+
     }
     // Start is called before the first frame update
     void Start () {
