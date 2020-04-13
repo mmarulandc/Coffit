2403a9ab79bc6e5eb5fe4fce92acab5461c29a74
yeshua19pro <yeshuaangelito@gmail.com>
2020-04-12 20:57:49 -0500

    Agregando nuevos scripts, corrijiendo la masa y el tamano de la cancha

---
+++ Coffit/Assets/Scripts/Gol.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Gol : MonoBehaviour {
+    public Vector3 startPosition;
+    public int cont = 0;
+
+    void OnTriggerEnter (Collider other) {
+        if (cont == 0) {
+            print ("Reset");
+            startPosition = new Vector3 (0f, 2f, 5f);
+            transform.position = startPosition;
+            cont++;
+        }
+
+    }
+    void OnTriggerExit (Collider other) {
+      
+        cont = 0;
+    }
+    // Start is called before the first frame update
+    void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+    }
+}
---
+++ Coffit/Assets/Scripts/Reset.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reset : MonoBehaviour {
+
+    public Vector3 startPosition;
+
+    // Start is called before the first frame update
+    void Start () {
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+
+        if (Input.GetKey ("r")) {
+            print ("Reset");
+            startPosition = new Vector3 (0f, 2f, 5f);
+            transform.position = startPosition;
+        }
+    }
+
+    void Inicio () {
+        startPosition = transform.position;
+    }
+}
